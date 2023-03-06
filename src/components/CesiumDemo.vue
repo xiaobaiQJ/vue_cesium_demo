46da41878b2b24ccb69f1c167494a14f28b84a89
Ztdata--niubao <niubao@chitechdata.cn>
2023-03-06 12:58:27 +0800

    1

---
+++ src/components/CesiumDemo.vue
@@ -0,0 +1,67 @@
+<template>
+  <div :id="id" ref="cesiumContainer" :style="{ height, width }" />
+</template>
+
+<script>
+import 'cesium/Build/Cesium/Widgets/widgets.css'
+import { Viewer } from 'cesium'
+
+export default {
+  name: 'CesiumMap',
+  props: {
+    id: {
+      type: String,
+      default: (() => Math.random().toString(36).substr(2))()
+    },
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '100%'
+    }
+  },
+  data() {
+    return {
+      viewer: null
+    }
+  },
+  mounted() {
+    this.initCesium()
+  },
+  beforeDestroy() {
+    if (this.viewer) {
+      this.viewer.destroy()
+      this.viewer = null
+    }
+  },
+  methods: {
+    initCesium() {
+      const el = this.$refs.cesiumContainer
+
+      this.viewer = new Viewer(el, {
+        selectionIndicator: false,
+        infoBox: false,
+        contextOptions: {
+          // 硬件反走样，默认值为 1
+          msaaLevel: 8,
+          requestWebgl2: true
+        },
+        animation: false,
+        timeline: false, // 底部时间线
+        fullscreenButton: false, // 全屏
+        vrButton: false, // VR
+        sceneModePicker: false, // 选择视角的模式（球体、平铺、斜视平铺）
+        baseLayerPicker: false, // 图层选择器（地形影像服务）
+        navigationHelpButton: false, // 导航帮助(手势，鼠标)
+        geocoder: false, // 位置查找工具
+        homeButton: false // 视角返回初始位置
+      })
+      // 去除logo
+      this.viewer.cesiumWidget.creditContainer.style.display = "none";
+
+    }
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CesiumDemo />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CesiumDemo from '@/components/CesiumDemo.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    CesiumDemo
   }
 }
 </script>
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+}
+</style>
